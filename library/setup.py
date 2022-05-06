ad1d36d74666dba8fc5d21fffc847e3067c64ce5
EvangMM <matteomartini92@yahoo.it>
2022-05-06 11:16:53 +0200

    updated setup.py

--- library/setup.py
+++ library/setup.py
@@ -1,4 +1,22 @@
+import sys
 from setuptools import setup, find_packages
+
+install_requires = [
+    "azure-storage-blob==12.11.0",
+    "boto3==1.22.8",
+    "botocore==1.25.8",
+    "psycopg2-binary==2.9.3",
+    "pyarrow==7.0.0",
+    "pydantic==1.9.0",
+    "pyodbc==4.0.32",
+    "requests==2.27.1",
+    "SQLAlchemy==1.4.36",
+]
+
+if sys.version_info[1] == 7:
+    install_requires.append("pandas==1.3.5")
+else:
+    install_requires.append("pandas==1.4.2")
 
 setup(
     name="datajudge",
@@ -6,21 +24,12 @@ setup(
     author="",
     author_email="",
     description="A framework to monitor the data validation process.",
-    install_requires=[
-            "requests>=2.25.1",
-            "boto3>=1.17.5",
-            "botocore>=1.20.5",
-            "pandas>=1.2.4",
-            "azure-storage-blob>=12.8.1"
-            ],
+    install_requires=install_requires,
     extras_require={
         "all": [
-            "frictionless>=4.9.0",
-            "duckdb>=0.3.4",
-            "pandas-profiling>=2.11.0",
-            ],
-        "frictionless": [
-            "frictionless>=4.9.0",
+            "duckdb==0.3.4",
+            "frictionless==4.32.0",
+            "pandas-profiling==3.2.0",
             ],
     },
     packages=find_packages()
